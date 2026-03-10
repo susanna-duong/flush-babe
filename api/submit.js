a3d2b5fe97f9d3630f39efa59a2f47d18b3dec56
susanna-duong <susanna.duong@gmail.com>
2026-03-09 20:15:04 -0400

    updated status

--- api/submit.js
+++ api/submit.js
@@ -27,7 +27,6 @@ export default async function handler(req, res) {
               'Address': address || '',
               'Notes': notes || '',
               'Submitted At': new Date().toISOString().split('T')[0],
-              '_Status': 'Pending'
             }
           })
         }
